78165d3707fdeb9b3f58bd1fe63ec0e12d052a6b
procheta1999 <bhattacharyya.procheta1999@gmail.com>
2020-10-29 20:48:32 +0530

    all changes made

--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "003ddcf5a8a7c65e83e7";
+/******/ 	var hotCurrentHash = "f8cc1c581c461a594abe";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
